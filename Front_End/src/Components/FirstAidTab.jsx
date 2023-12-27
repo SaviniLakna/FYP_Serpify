bae674f1b16492a8c03b466e7fc1ce51a9519d44
Chanu Perera <86464186+SaviniLakna@users.noreply.github.com>
2023-12-27 20:03:51 +0530

    Update FirstAidTab.jsx

--- Front_End/src/Components/FirstAidTab.jsx
+++ Front_End/src/Components/FirstAidTab.jsx
@@ -100,13 +100,116 @@ export default function FullWidthTabs() {
             <div className="w-full md:w-1/2 text-left flex flex-col relative justify-center items-center">
 <img src={Medic} alt="medicine" className="lg:w-[320px] md:w-[320px] w-full" />
             </div>
+
+           
           </div>
+          <div className="w-full text-left text-[#3d3d3d]">
+          <h2 className="text-[1.2rem] font-semibold mb-10">
+                To do list
+              </h2>
+            <ol className="flex flex-col list-decimal space-y-3" >
+              <li>The commonest reaction following snakebite is fear — the victim thinks of death. Reassurance is vital and should be done in a positive and authoritative manner and 
+                continued for as long as necessary. The following points should be emphasised :
+                
+
+                <ul className="list-disc ml-10">
+                <li>
+                Most snakes are not venomous.
+                </li>
+                <li>
+                Even if a biting snake is venomous, it may not inject venom.
+                </li>
+                <li>
+                The presence of fang marks alone does not mean that venom has entered the body.
+                </li>
+                <li>
+                If envenoming has taken place, affective treatment is available in hospitals and full recovery is likely.
+                </li>
+
+
+              </ul>
+
+                </li>
+              
+              <li>
+              To remove venom, which may lie on the surface of the skin, the bitten area should be washed gently with soap and water, or wiped with a wet cloth.
+              </li>
+
+              <li>
+              Swelling of the bitten limb is a common feature after venomous snakebite. Prevent complications by removing, as early as possible, rings, bangles, anklets, cords or clothing, which could cause constriction if swelling were to occur.
+              </li>
+
+
+              <li>
+              Take the victim to hospital as early as possible. An accurate description of the circumstances of the bite will facilitate the identification of the snake. Photographs taken with a phone will be very helpful to identify the snake. Accurate identification will influence the management of the patient. It is no longer necessary to bring the biting snake to hospital for proper management. Live snakes, as well as dead ones, should be handled carefully.
+              </li>
+
+              <li>
+              If the victim has difficulty in breathing, particularly after krait bite, provision of artificial breathing on the way to hospital either by mouth to mouth or bag and mask ventilation, if you have the capability to do so, is a lifesaving first aid intervention.
+              </li>
+            </ol>
+
+          </div>
+
+
+
+          <div className="w-full text-left text-[#3d3d3d] mt-10">
+          <h2 className="text-[1.2rem] font-semibold mb-5">
+                <span className="text-red-500">Not to do  </span>list
+              </h2>
+            <ol className="flex flex-col list-decimal space-y-3" >
+              <li>Do not panic and be tempted to do anything detrimental to the victim.
+
+                </li>
+              
+              <li>
+              Alcohol hastens circulation of venom and should not be given.
+              </li>
+
+              <li>
+              Do not apply tourniquets in the first aid treatment of snakebite. Applications of “pressure bandages”, as recommended in some guidelines, is also not recommended. The effectiveness of pressure bandages for bites of Sri Lankan snakes has not been proven.
+              </li>
+
+
+              <li>
+              Incision of or application of suction to the bite wound is not advisable. Snakes usually inject venom too deeply for suction to be of any use. Inexpert incisions may damage tendons, blood vessels and nerves; bleeding from these wounds and infection may create added problems.
+              </li>
+
+              <li>
+              Thambili(King coconut) and kurumba water and fruit juices should not be given. They contain potassium, which can cause problems if kidney damage occurs following snakebite.
+              </li>
+
+
+              <li>
+              Aspirin may produce persistent bleeding from the stomach, especially following Russell’s viper bite and should not be given.
+              </li>
+
+
+            </ol>
+
+          </div>
+
+
+
         </FistAidTab>
+
+
+
+
+
+
+
+
+
+
+
+
+
         <FistAidTab value={value} index={1} dir={theme.direction}>
-          සිංහල Body
+        නඩත්තු කටයුතු හේතුවෙන් වසා ඇත
         </FistAidTab>
         <FistAidTab value={value} index={2} dir={theme.direction}>
-          தமிழ் Body
+        සිංහල තෙරියා ද?
         </FistAidTab>
       </SwipeableViews>
     </Box>
